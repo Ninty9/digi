60619ea3864b6b4143c10eae73f12ce5496b088f
Ninty <ninty0808@gmail.com>
2024-04-08 16:05:54 +0200

    tree

--- Dialogue.cs
+++ Dialogue.cs
@@ -14,26 +14,25 @@ public partial class Dialogue : Control
 	public static DialogueTree CurrentTree;
 
 	[Export] private RichTextLabel dia;
+	[Export] private ColorRect diaSel;
+	[Export] private float selSpeed;
 	[Export] private RichTextLabel cha;
 	[Export] private TextureRect img;
-	[Export] private VBoxContainer optionContianer;
-	private RichTextLabel[] optionLabels;
-	
 	[Export] private Profile[] profiles;
 
+	private string[] options = new string[6];
+	
+	public static bool WantsInput;
+	private float selHeight;
+	
 	private List<string> inputs = new ();
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		optionLabels = new RichTextLabel[5];
-		int i = 0;
-		foreach (var n in optionContianer.GetChildren())
-			optionLabels[i++] = n.GetNode<RichTextLabel>(".");
-		
 		d = this;
 	}
 	
-	public void OnDialogueOutput(String dialogue, String character, Array parameters)
+	public void OnDialogueOutput(string dialogue, string character, Array parameters)
 	{
 		Visible = true;
 		dia.Text = dialogue;
@@ -46,15 +45,52 @@ public partial class Dialogue : Control
 			}
 		}
 	}
+	
+	public void OnDialogueOutput(string[] dialogue, string character, Array parameters)
+	{
+		string newD = "";
+		foreach (var i in inputs)
+		{
+			if (newD == "")
+				newD += i;
+			else
+				newD += "\n" + i;
+
+		}
+		OnDialogueOutput(newD, character, parameters);
+	}
+
+	public override void _Process(double delta)
+	{
+		if(!WantsInput) return;
+		if (Input.IsActionJustPressed("Interact"))
+		{
+			diaSel.Visible = true;
+			GD.Print("bint");
+		}
+		if (Input.IsActionPressed("Interact"))
+		{
+			GD.Print("ed");
+			diaSel.Position = diaSel.Position with
+			{
+				Y = (diaSel.Position.Y + selSpeed * (float)delta) % (dia.Size.Y - 10)
+			};
+		}
+		if (Input.IsActionJustReleased("Interact"))
+		{
+			// WantsInput = false;
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		// CheckInput();
+		CheckInput();
 	}
 
 	private void CheckInput()
 	{
 		if(CurrentTree == null) return;
+		GD.Print(CurrentTree.GetInputOptions().Length);
 		if(inputs.Count != 0) return;
 		if(CurrentTree.GetInputOptions().Length == 0) return;
 		inputs = new List<string>();
@@ -64,17 +100,13 @@ public partial class Dialogue : Control
 			inputs.Add(input.Input);
 		}
 		if(inputs.Count == 0) return;
-		GD.Print("banzingr");
-		string options = "";
-		foreach (var i in inputs)
+		for (var i = 0; i < inputs.Count; i++)
 		{
-			if (options == "")
-				options += i;
-			else
-				options += "\n" + i;
-
+			options[i] = inputs[i];
 		}
+
 		OnDialogueOutput(options, "bean", null);
+		WantsInput = true;
 	}
 	
 	public static void Start(DialogueTree tree)
